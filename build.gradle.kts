feec852d00e99e9e5babaa49c6d7f20dfac98e16
Paulem <66211574+Paulem79@users.noreply.github.com>
2025-08-10 18:54:04 +0200

    fix(ci): fixed jpackage ci

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,11 @@
+import org.gradle.internal.jvm.Jvm
+import org.panteleyev.jpackage.ImageType
+import org.panteleyev.jpackage.JPackageTask
+
 plugins {
     java
     application
+    id("org.panteleyev.jpackageplugin") version "1.7.3"
 }
 
 group = "ovh.paulem.mc"
@@ -10,14 +15,11 @@ repositories {
     mavenCentral()
 }
 
-val lwjglVersion = "3.3.1"
-val jomlVersion = "1.10.5"
-
 dependencies {
-    implementation("org.joml:joml:$jomlVersion")
+    implementation("org.joml:joml:1.10.5")
 
     // BOM pour gérer les versions LWJGL
-    implementation(platform("org.lwjgl:lwjgl-bom:$lwjglVersion"))
+    implementation(platform("org.lwjgl:lwjgl-bom:3.3.1"))
 
     // Artefacts Java
     implementation("org.lwjgl:lwjgl")
@@ -37,7 +39,7 @@ application {
     mainClass.set(project.group.toString() + ".Main")
 }
 
-tasks.jar {
+tasks.withType<Jar>().configureEach {
     manifest {
         attributes(mapOf("Main-Class" to application.mainClass.get()))
     }
@@ -49,7 +51,6 @@ java {
     }
 }
 
-// Optionnel: fat jar. Attention: il ne packagera pas automatiquement les natives par plateforme.
 tasks.register<Jar>("fatJar") {
     duplicatesStrategy = DuplicatesStrategy.EXCLUDE
     from(sourceSets.main.get().output)
@@ -59,4 +60,87 @@ tasks.register<Jar>("fatJar") {
         attributes(mapOf("Main-Class" to application.mainClass.get()))
     }
     archiveClassifier.set("all")
+}
+
+// --- JPACKAGE ---
+val jvmOpts = listOf("-Dfile.encoding=UTF-8", "--add-exports=javafx.graphics/com.sun.glass.ui=ALL-UNNAMED", "--add-opens=javafx.graphics/javafx.scene.layout=ALL-UNNAMED")
+
+tasks.withType<JPackageTask>().configureEach {
+    dependsOn("fatJar")
+
+    appName = rootProject.name
+    appVersion = project.version.toString()
+    vendor = "Paulem"
+    copyright = "Copyright (c) 2025 Paulem"
+    runtimeImage = Jvm.current().javaHome
+    destination = layout.projectDirectory.dir("dist")
+    input = layout.buildDirectory.dir("libs")
+    mainJar = (tasks.getByName("fatJar") as Jar).archiveFileName.get()
+    mainClass = application.mainClass.get()
+    javaOptions = jvmOpts
+}
+
+var infra = ""
+tasks.register<JPackageTask>("zipjpackage") {
+    group = tasks.jpackage.get().group
+
+    type = ImageType.APP_IMAGE
+
+    linux {
+        infra = "linux"
+    }
+
+    mac {
+        icon = layout.projectDirectory.dir("src").dir("main").dir("resources").dir("assets").file("icons.icns")
+        infra = "macos"
+    }
+
+    windows {
+        icon = layout.projectDirectory.dir("src").dir("main").dir("resources").dir("assets").file("icons.ico")
+
+        winConsole = true
+        infra = "windows"
+    }
+
+    finalizedBy("renameZip")
+}
+
+tasks.register<Zip>("renameZip") {
+    group = tasks.jpackage.get().group
+    archiveFileName.set(infra + "-FlowJsonCreator-" + project.version + ".zip")
+    destinationDirectory.set(layout.projectDirectory.dir("dist"))
+
+    from(layout.projectDirectory.dir("dist/FlowJsonCreator"))
+}
+
+tasks.jpackage {
+    linux {
+        type = ImageType.DEB
+    }
+
+    mac {
+        icon = layout.projectDirectory.dir("src").dir("main").dir("resources").dir("assets").file("icons.icns")
+
+        type = ImageType.DMG
+    }
+
+    windows {
+        icon = layout.projectDirectory.dir("src").dir("main").dir("resources").dir("assets").file("icons.ico")
+
+        type = ImageType.MSI
+
+        winConsole = true
+        if(type.get() == ImageType.EXE || type.get() == ImageType.MSI) {
+            winMenu = true
+            winDirChooser = true
+            winPerUserInstall = true
+            winShortcut = true
+            winShortcutPrompt = true
+            // winUpdateUrl can be interesting for auto-updates
+        }
+    }
+}
+
+tasks.clean {
+    dependsOn("deleteDist")
 }
